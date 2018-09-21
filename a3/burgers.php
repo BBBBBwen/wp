c5d0dcbbbe579ef5a8f79867147940cea3c2a908
bowen zhang <s3617571@student.rmit.edu.au>
2018-09-21 16:55:28 +1000

    menu

---
+++ a3/burgers.php
@@ -0,0 +1,29 @@
+<?php
+  require_once('tools.php'); 
+    top_module("products");
+    top_nav();
+    sign();
+?>
+<main class='area'>
+    <div class='dis-flex'>
+    <aside class='category'>
+            <ul class="menu">
+                <li class="menu"><a href="products.php" hreflang="en">Full Menu</a></li>
+                <li class="menu"><a href="boxed-meals.php" hreflang="en">Boxed Meals</a></li>
+                <li class="menu"><a href="burgers.php" hreflang="en">Burgers</a></li>
+                <li class="menu"><a href="sides.php" hreflang="en">Sides</a></li>
+            </ul>
+    </aside>
+    <div class='products'>
+        <span class='pro'>
+            <a href="burger.php" target='_top'>
+                <img src='../../A3media/Fish Burger.jpg' alt='FishBurger' class='proImg' />
+            </a>
+            <h3>FishBurger</h3>
+        </span>
+    </div>
+</div>
+</main>
+<?php
+end_module(); 
+?>
--- a3/products.php
+++ a3/products.php
@@ -6,38 +6,36 @@
 ?>
 <main class='area'>
     <div class='dis-flex'>
-    <aside class='category'>
+        <aside class='category'>
             <ul class="menu">
-                <li class="menu"><a href="/menu/chicken" hreflang="en">Chicken</a></li>
-                <li class="menu"><a href="/menu/boxed-meals" hreflang="en">Boxed Meals</a></li>
-                <li class="menu"><a href="/menu/burgers" hreflang="en">Burgers</a></li>
-                <li class="menu"><a href="/menu/snacks" hreflang="en">Snacks</a></li>
-                <li class="menu"><a href="/menu/sides" hreflang="en">Sides</a></li>
-                <li class="menu"><a href="/menu/drinks" hreflang="en">Drinks</a></li>
+                <li class="menu"><a href="products.php" hreflang="en">Full Menu</a></li>
+                <li class="menu"><a href="boxed-meals.php" hreflang="en">Boxed Meals</a></li>
+                <li class="menu"><a href="burgers.php" hreflang="en">Burgers</a></li>
+                <li class="menu"><a href="sides.php" hreflang="en">Sides</a></li>
             </ul>
-    </aside>
-    <div class='products'>
-        <span class='pro'>
-            <a href="chips.php" target='_top'>
-                <img src='../../A3media/Chips.jpg' alt='Chips' class='proImg' />
-            </a>
-            <h3>Chips</h3>
+        </aside>
+        <div class='products'>
+            <span class='pro'>
+                <a href="chips.php" target='_top'>
+                    <img src='../../A3media/Chips.jpg' alt='Chips' class='proImg' />
+                </a>
+                <h3>Chips</h3>
 
-        </span>
-        <span class='pro'>
-            <a href="burger.php" target='_top'>
-                <img src='../../A3media/Fish Burger.jpg' alt='FishBurger' class='proImg' />
-            </a>
-            <h3>FishBurger</h3>
-        </span>
-        <span class='pro'>
-            <a href="fishnchips.php" target='_top'>
-                <img src='../../A3media/Fish&Chips.jpg' alt='Fish&Chips' class='proImg' />
-            </a>
-            <h3>Fish&Chips</h3>
-        </span>
+            </span>
+            <span class='pro'>
+                <a href="burger.php" target='_top'>
+                    <img src='../../A3media/Fish Burger.jpg' alt='FishBurger' class='proImg' />
+                </a>
+                <h3>FishBurger</h3>
+            </span>
+            <span class='pro'>
+                <a href="fishnchips.php" target='_top'>
+                    <img src='../../A3media/Fish&Chips.jpg' alt='Fish&Chips' class='proImg' />
+                </a>
+                <h3>Fish&Chips</h3>
+            </span>
+        </div>
     </div>
-</div>
 </main>
 <?php
 end_module(); 
